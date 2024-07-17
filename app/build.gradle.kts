90944080f2d8238e5ee2e9d8097bcae3b4fed0f0
Usuario <yandres24@hotmail.com>
2024-07-17 02:57:13 -0500

    fix google service and gradle version compile sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.yaml.pizzeriashopunal"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.yaml.pizzeriashopunal"
